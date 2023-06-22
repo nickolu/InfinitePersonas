d99ff9be574d3038b94ade8772cb446090f51f0b
Nick Cunningham <nick.cunningham@leadcrunch.ai>
2023-06-22 16:25:40 -0700

    add retry logic

--- src/components/hooks/useCharacterChat.ts
+++ src/components/hooks/useCharacterChat.ts
@@ -3,21 +3,64 @@ import {use, useCallback, useEffect, useState} from 'react';
 import Character from '../../core/Character';
 import Message, {UserMessage} from '@/core/Message';
 
+const RESET_TIMEOUT_MS = 1000;
+
+function shouldRetry(
+  error: any,
+  attempt: number,
+  maxRetry: number
+): {retry: boolean; message: string} {
+  const defaultMessage = 'An unknown error occurred';
+
+  if (!error.response) {
+    return {retry: false, message: error.message || defaultMessage};
+  }
+
+  if (error.response.status >= 500 && error.response.status < 600) {
+    const errorMessage = `Error: ${error.message || defaultMessage}`;
+
+    if (attempt >= maxRetry) {
+      return {
+        retry: false,
+        message: `${errorMessage}. Maximum retry attempts exceeded.`,
+      };
+    }
+
+    return {retry: true, message: errorMessage};
+  }
+
+  return {retry: false, message: error.message || defaultMessage};
+}
+
 async function getNextChatMessage(
   messages: Message[],
-  character: Character
+  character: Character,
+  maxRetry: number = 3
 ): Promise<{text: string}> {
   const apiUrl = `/api/LLM/characterChat`;
-  try {
-    const response = await axios.post(apiUrl, {params: {character, messages}});
-    return response.data;
-  } catch (error: any) {
-    console.log(error);
-    const errorMessage = error.message
-      ? error.message
-      : 'An unknown error occurred';
-    return {text: `Error: ${errorMessage}`};
+
+  let attempt = 0;
+
+  while (attempt < maxRetry) {
+    try {
+      const response = await axios.post(apiUrl, {
+        params: {character, messages},
+      });
+      return response.data;
+    } catch (error: any) {
+      const {retry, message} = shouldRetry(error, attempt, maxRetry);
+
+      if (retry) {
+        await new Promise((res) => setTimeout(res, RESET_TIMEOUT_MS));
+        attempt++;
+      } else {
+        console.log(error);
+        return {text: `Error: ${message}`};
+      }
+    }
   }
+
+  return {text: 'Error: Unexpected error'};
 }
 
 export function useWelcomeMessage(
